43519a1960e43cd1b0c2123bfe7461f9d5299619
Eric Avdey <eiri@eiri.ca>
2026-02-09 17:56:34 -0400

    feat: parse args with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = "Generator of fake pronounceable words")]
+struct Args {
+    /// number of words to generate
+    #[arg(short, long, default_value_t = 1)]
+    number: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for _ in 0..args.number {
+        println!("{}", String::from("flabild"));
+    }
 }
